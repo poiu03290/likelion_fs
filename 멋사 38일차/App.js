87ac732b6125648fa06882a8018725324b744287
Hyuk Kwon <82810259+poiu03290@users.noreply.github.com>
2022-01-11 17:32:30 +0900

    멋사_프론트엔드스쿨_38일차_React실습자료

---
+++ 멋사 38일차/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './components/Greeting.js';
+import Login from './components/Login.js';
+
+function App() {
+  return (
+    <div>
+      <Greeting/>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
